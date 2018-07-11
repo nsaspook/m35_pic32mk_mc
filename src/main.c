58d68211290e8239908fe722f032a02036003d4e
root <root@newimp.microchip.com>
2018-07-10 17:46:34 -0700

    updates

---
+++ src/main.c
@@ -0,0 +1,116 @@
+/*******************************************************************************
+  MPLAB Harmony Project Main Source File
+
+  Company:
+    Microchip Technology Inc.
+  
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for an MPLAB Harmony project.
+
+  Description:
+    This file contains the "main" function for an MPLAB Harmony project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state 
+    machines of all MPLAB Harmony modules in the system and it calls the 
+    "SYS_Tasks" function from within a system-wide "super" loop to maintain 
+    their correct operation. These two functions are implemented in 
+    configuration-specific files (usually "system_init.c" and "system_tasks.c")
+    in a configuration-specific folder under the "src/system_config" folder 
+    within this project's top-level folder.  An MPLAB Harmony project may have
+    more than one configuration, each contained within it's own folder under
+    the "system_config" folder.
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2013-2014 released Microchip Technology Inc.  All rights reserved.
+
+//Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "system/common/sys_module.h"   // SYS function prototypes
+#include "system_config/default/framework/driver/usart/drv_usart_static.h"
+#include "m35qei.h"
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+/* redirect printf to uart3 */
+void _mon_putc(char c)
+{
+	while (DRV_USART0_TransmitBufferIsFull()) {
+	}; //Wait till transmission is complete
+	DRV_USART0_WriteByte(c);
+}
+
+int main(void)
+{
+	int32_t itest, update_count = 0;
+	/* Initialize all MPLAB Harmony modules, including application(s). */
+	SYS_Initialize(NULL);
+
+	/* serial clock is off, this is actually 76.8 kb/s */
+	DRV_USART0_BaudSet(115200);
+	
+	while (true) {
+		/* Maintain state machines of all polled MPLAB Harmony modules. */
+		SYS_Tasks();
+		
+		/* update local value of the position counter */
+		itest = POS1CNT;
+
+		if (update_count++ > 20480) {
+			/* flash the board leds using the position counter bits */
+			LATGbits.LATG12 = itest >> 10;
+			LATGbits.LATG13 = itest >> 12;
+			LATGbits.LATG14 = itest >> 14;
+			/* send to uart3 the current POS1CNT value */
+			printf("count %8i\r\n", itest);
+			update_count = 0;
+		}
+	}
+
+	/* Execution should not come here during normal operation */
+
+	return( EXIT_FAILURE);
+}
+
+
+/*******************************************************************************
+ End of File
+ */
+
